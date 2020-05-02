2ac34610a232347f73fee75ef6a6093a13f4bece
Alejandro R. Sedeño <asedeno@mit.edu>
2020-05-01 21:02:06 -0400

    gssapi settings - add daemon keytab info

--- roost_ng/settings/gssapi.py
+++ roost_ng/settings/gssapi.py
@@ -1,6 +1,11 @@
 import os
 
 # TODO: parameterize this
+# This keytab is used when talking to the API.
 DEFAULT_KRB5_KTNAME = '/etc/krb5.keytab.HTTP'
+
+# This keytab is used by the subscriber when talking to a zephyrd.
+DEFAULT_SUBSCRIBER_KRB5_KEYTAB = '/etc/krb5.keytab.daemon'
+
 if DEFAULT_KRB5_KTNAME and not os.environ.get('KRB5_KTNAME'):
     os.environ['KRB5_KTNAME'] = DEFAULT_KRB5_KTNAME
