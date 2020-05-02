8298406193936366ee9345398a0d5d61a81024a7
Alejandro R. Sedeño <asedeno@mit.edu>
2020-05-02 00:13:04 -0400

    factor out local django channels layers implementation
    This moves the channels layers implementation into a mixin, and then
    adds it to the overseer as well, so it can respond to messages about
    new users.

--- roost_backend/user_process.py
+++ roost_backend/user_process.py
@@ -18,6 +18,52 @@ import setproctitle
 _LOGGER = logging.getLogger(__name__)
 
 
+class _ChannelLayerMixin:
+    """This mixin can be used to add Django Channels Layers support to a class.  To ues it, inherit
+    from it and define a property `group_names` of no arguments that returns a list of groups
+    to subscribe to. Then start a task to run the `channel_layer_handler`, cancel it when you
+    want to stop. This may be worth extracting to a utility module.
+    """
+    def __init__(self):
+        self.channel_layer = None
+        self.channel_name = None
+        self.channel_receive = None
+
+    @property
+    def group_names(self):
+        raise NotImplementedError()
+
+    async def channel_layer_handler(self):
+        try:
+            # Initialize channel layer.
+            self.channel_layer = channels.layers.get_channel_layer()
+            self.channel_name = await self.channel_layer.new_channel()
+            self.channel_receive = functools.partial(self.channel_layer.receive, self.channel_name)
+
+            # Subscribe to groups
+            for group_name in self.group_names:
+                await self.channel_layer.group_add(group_name, self.channel_name)
+
+            # Wait for and dispatch messages until we get cancelled.
+            while True:
+                try:
+                    await channels.utils.await_many_dispatch([self.channel_receive], self.dispatch)
+                except ValueError as exc:
+                    _LOGGER.error('Dispatch failed: %s', exc)
+        except asyncio.CancelledError:
+            pass
+
+    async def dispatch(self, message):
+        # Let's use the same dispatching mechanism that django channels consumers use.
+        handler_name = channels.consumer.get_handler_name(message)
+        handler = getattr(self, handler_name, None)
+
+        if handler:
+            await handler(message)
+        else:
+            raise ValueError(f'No handler for message type "{message["type"]}"')
+
+
 class Manager:
     """This class is used by an outside caller to start and stop the set
     of user processes."""
@@ -48,7 +94,7 @@ class Manager:
         return ret
 
 
-class Overseer:
+class Overseer(_ChannelLayerMixin):
     """This class is forked by the Manager class. It is responsible for
     forking off the individual user processes and restarting them if
     necessary, as well as for telling them to stop upon from request
@@ -56,6 +102,7 @@ class Overseer:
     # TODO: make this more async
     # TODO: hook into channels layer to alert about new/deleted users.
     def __init__(self, stop_event, start=True):
+        super().__init__()
         signal.signal(signal.SIGINT, signal.SIG_IGN)
         signal.signal(signal.SIGTERM, signal.SIG_DFL)
         self.stop_event = stop_event
@@ -67,6 +114,10 @@ class Overseer:
 
     def __str__(self):
         return f'Overseer<{self.pid}>'
+
+    @property
+    def group_names(self):
+        return ['UP_OVERSEER']
 
     def start(self):
         if not settings.configured:
@@ -81,11 +132,17 @@ class Overseer:
         asyncio.run(self.oversee())
 
     async def oversee(self):
+        channel_task = asyncio.create_task(self.channel_layer_handler())
         for princ, task in self.user_tasks.items():
             if task is None:
                 self.user_tasks[princ] = asyncio.create_task(self.user_process_watcher(princ))
 
+        # We could just wait for the async tasks to finish, but then
+        # we would not be waiting on any tasks for users created after
+        # start-up, once we handle dynamic user creation.
+        await sync_to_async(self.stop_event.wait)()
         await asyncio.wait([task for task in self.user_tasks.values() if task is not None])
+        channel_task.cancel()
 
     async def user_process_watcher(self, principal):
         while not self.stop_event.is_set():
@@ -93,30 +150,36 @@ class Overseer:
             proc.start()
             await sync_to_async(proc.join)()
 
+    # Start of Channel Layer message handlers
+    async def add_user(self, message):
+        # {'type': 'add_user', 'principal': '<principal of new user>'}
+        # Spawns user process for user if not already running.
+        princ = message['principal']
+        if princ not in self.user_tasks:
+            self.user_tasks[princ] = asyncio.create_task(self.user_process_watcher(princ))
+    # End message handlers
 
-class UserProcess:
+
+class UserProcess(_ChannelLayerMixin):
     """
     Kerberos and zephyr are not particularly threadsafe, so each user
     will have their own process.
     """
 
     def __init__(self, principal, stop_event, start=True):
+        super().__init__()
         self.principal = principal
-        self.group_name = self.principal_to_group_name(principal)
         self.stop_event = stop_event
-        self.channel_layer = None
-        self.channel_name = None
-        self.channel_receive = None
         if start:
             self.start()
 
     def __str__(self):
         return f'UserProcess<{self.principal}>'
 
-    @staticmethod
-    def principal_to_group_name(principal):
-        b64_principal = base64.b64encode(principal.encode("utf-8")).decode("ascii")
-        return f'PRINC_{b64_principal.strip("=")}'
+    @property
+    def group_names(self):
+        b64_principal = base64.b64encode(self.principal.encode("utf-8")).decode("ascii")
+        return [f'PRINC_{b64_principal.strip("=")}']
 
     def start(self):
         setproctitle.setproctitle(f'UP/{self.principal}')
@@ -128,38 +191,6 @@ class UserProcess:
         await sync_to_async(self.stop_event.wait)()
         channel_task.cancel()
         zephyr_task.cancel()
-
-    async def channel_layer_handler(self):
-        _LOGGER.debug('faux channel layer handler started.')
-        try:
-            # Initialize channel layer.
-            self.channel_layer = channels.layers.get_channel_layer()
-            self.channel_name = await self.channel_layer.new_channel()
-            self.channel_receive = functools.partial(self.channel_layer.receive, self.channel_name)
-
-            # Subscribe to groups
-            await self.channel_layer.group_add(self.group_name, self.channel_name)
-
-            # Wait for and dispatch messages until we get cancelled.
-            while True:
-                try:
-                    await channels.utils.await_many_dispatch([self.channel_receive], self.dispatch)
-                except ValueError as exc:
-                    _LOGGER.error('Dispatch failed: %s', exc)
-        except asyncio.CancelledError:
-            _LOGGER.debug('faux channel layer handler cancelled.')
-        finally:
-            _LOGGER.debug('faux channel layer handler done.')
-
-    async def dispatch(self, message):
-        # Let's use the same dispatching mechanism that django channels consumers use.
-        handler_name = channels.consumer.get_handler_name(message)
-        handler = getattr(self, handler_name, None)
-
-        if handler:
-            await handler(message)
-        else:
-            raise ValueError(f'No handler for message type "{message["type"]}"')
 
     # Start of Channel Layer message handlers
     async def test(self, message):
