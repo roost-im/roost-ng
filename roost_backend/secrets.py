bc083dd7d788ea5f6400147b4816e75f2f379adc
Alejandro R. Sedeño <asedeno@mit.edu>
2020-09-07 15:41:47 -0400

    initial support for message id sealing

--- roost_backend/secrets.py
+++ roost_backend/secrets.py
@@ -3,15 +3,14 @@ import uuid
 
 from django.conf import settings
 
-
+_BASE_KEY = hashlib.blake2s(settings.SECRET_KEY.encode('utf-8'), digest_size=16).digest()
 def _secret_generator(context, as_uuid=False):
     """Derive secrets from SECRET_KEY and a context string"""
-    ret = uuid.uuid5(
-        uuid.UUID(bytes=hashlib.blake2s(settings.SECRET_KEY.encode('utf-8'), digest_size=16).digest()),
-        context)
+    ret = uuid.uuid5(uuid.UUID(bytes=_BASE_KEY), context)
     if as_uuid:
         return ret
     return str(ret)
 
 
 AUTHTOKEN_KEY = _secret_generator('auth')
+MESSGE_ID_SEALING_KEY = _secret_generator('msg.id', as_uuid=True).bytes
--- roost_backend/utils/__init__.py
+++ roost_backend/utils/__init__.py
@@ -1,7 +1,38 @@
+import uuid
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
 from . import kerberos
+from ..secrets import MESSGE_ID_SEALING_KEY
+
 
 def principal_to_user_process_group_name(princ):
     return kerberos.principal_to_group_name(princ, 'UP')
 
+
 def principal_to_user_socket_group_name(princ):
     return kerberos.principal_to_group_name(princ, 'WS')
+
+
+# Yes, for sealing and unsealing we are using AES in ECB mode.
+# Yes, this is exactly what we want.
+# id -> consistent UUID, and back again
+
+# Quoting the original roost codebase on this topic (lib/msgid.js):
+# Yeah, yeah, ad-hoc crypto... this is not particularly
+# important. Just a way to give us opaque message ids without having
+# to actually maintain state, and the only purpose of opaque message
+# ids is to not trivially reveal how many messages you can't see.
+
+def seal_message_id(msg_id: int) -> uuid.UUID:
+    cipher = Cipher(algorithms.AES(MESSGE_ID_SEALING_KEY), modes.ECB())
+    enc = cipher.encryptor()
+    cbytes = enc.update(msg_id.to_bytes(16, 'big')) + enc.finalize()
+    return uuid.UUID(bytes=cbytes)
+
+
+def unseal_message_id(sealed_msg_id: uuid.UUID) -> int:
+    cipher = Cipher(algorithms.AES(MESSGE_ID_SEALING_KEY), modes.ECB())
+    dec = cipher.decryptor()
+    pbytes = dec.update(sealed_msg_id.bytes) + dec.finalize()
+    return int.from_bytes(pbytes, 'big')
