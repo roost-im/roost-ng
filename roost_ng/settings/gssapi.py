a33ab01abde1808a425921e283146aefc0585b33
Alejandro R. Sedeño <asedeno@mit.edu>
2020-09-13 20:47:10 -0400

    gssapi config

--- roost_ng/settings/gssapi.py
+++ roost_ng/settings/gssapi.py
@@ -1,11 +1,13 @@
 import os
 
-# TODO: parameterize this
+from . import config
+gssapi_config = config.get_config_for_module(__name__)
+
 # This keytab is used when talking to the API.
-DEFAULT_KRB5_KTNAME = '/etc/krb5.keytab.HTTP'
+DEFAULT_KRB5_KTNAME = gssapi_config.get('server_keytab', '/etc/krb5.keytab.HTTP')
 
 # This keytab is used by the subscriber when talking to a zephyrd.
-DEFAULT_SUBSCRIBER_KRB5_KEYTAB = '/etc/krb5.keytab.daemon'
+DEFAULT_SUBSCRIBER_KRB5_KEYTAB = gssapi_config.get('client_keytab', '/etc/krb5.keytab.daemon')
 
 if DEFAULT_KRB5_KTNAME and not os.environ.get('KRB5_KTNAME'):
     os.environ['KRB5_KTNAME'] = DEFAULT_KRB5_KTNAME
