42ebd1f3c33d9ec56acd149e876ee6dd8462784e
Alejandro R. Sedeño <asedeno@mit.edu>
2020-09-13 21:37:26 -0400

    prod: daphne with user subscriber wrapper

---
+++ misc/run_with_daphne.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import re
+import multiprocessing
+import sys
+
+from daphne.cli import CommandLineInterface
+
+
+def main():
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roost_ng.settings')
+
+    import roost_backend.subscribers
+
+    with roost_backend.subscribers.Manager():
+        sys.exit(CommandLineInterface.entrypoint())
+
+
+if __name__ == '__main__':
+    # This lives in misc; fix sys.path so it's like we're running at the project root.
+    sys.path = [os.path.join(sys.path[0], '..')] + sys.path[1:]
+    # Fork server lets us use multiprocessing without inheriting unwanted FDs.
+    multiprocessing.set_start_method('forkserver')
+    main()
