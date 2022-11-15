22bcab76ca0560045754371fb9986b10b563fa11
Alejandro R. Sedeño <asedeno@mit.edu>
2022-11-15 12:02:44 -0500

    Revert "We don't need early django setup anymore"
    This reverts commit 4cbbf42772965b175ba1f922c74237eedea9f821.
    
    We do still need this for misc/run_with_daphne.py

--- roost_ng/asgi.py
+++ roost_ng/asgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
+import os
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from roost_ng.middleware import DaphneRootPathForWebsockets
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roost_ng.settings')
+django.setup()
+
 import roost_ng.routing
+from roost_ng.middleware import DaphneRootPathForWebsockets
 
 
 application = ProtocolTypeRouter({
