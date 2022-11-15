a195991b56f879b1797cf19b46e712a6e4fce38d
Alejandro R. Sedeño <asedeno@mit.edu>
2022-11-15 11:46:22 -0500

    pylint: add file encoding

--- roost_ng/settings/config.py
+++ roost_ng/settings/config.py
@@ -7,7 +7,7 @@ configfile = os.getenv('ROOST_NG_CONFIGFILE', '/etc/roost-ng/config.yml')
 config = {}
 
 try:
-    with open(configfile, 'r') as f:
+    with open(configfile, 'r', encoding='utf8') as f:
         config = yaml.safe_load(f)
 except OSError:
     pass
--- roost_ng/settings/secrets.py
+++ roost_ng/settings/secrets.py
@@ -2,13 +2,15 @@ import os.path
 
 from . import config
 
+# pylint: disable=line-too-long
+
 # Set the Django SECRET_KEY, and potentially other secrets
 #
 # If /etc/roost-ng/django-SECRET_KEY exists, the entire contents will
 # be used as the key.
 
 # To take that approach, here's a one-liner you can use to initialize it:
-# python3 -c "import secrets; open('/etc/roost-ng/django-SECRET_KEY', 'w').write(secrets.token_urlsafe(50))
+# python3 -c "import secrets; open('/etc/roost-ng/django-SECRET_KEY', 'w', encoding='utf8').write(secrets.token_urlsafe(50))"
 
 # Otherwise, the SECRET_KEY will be fetched from /etc/roost-ng/config.yml if it's
 # set there, or otherwise use all-zeroes.
@@ -19,7 +21,7 @@ from . import config
 
 SECRET_KEY_FILE = '/etc/roost-ng/django-SECRET_KEY'
 if os.path.exists(SECRET_KEY_FILE):
-    with open(SECRET_KEY_FILE, 'r') as secret_key_file:
+    with open(SECRET_KEY_FILE, 'r', encoding='utf8') as secret_key_file:
         SECRET_KEY = secret_key_file.read()
 else:
     cfg = config.get_config_for_module(__name__)
