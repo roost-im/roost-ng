5484c1a6c156488e722565755ef1171afe3d31cb
Alejandro R. Sedeño <asedeno@mit.edu>
2024-01-06 15:06:51 -0500

    drop custom pylint init-hook due to deprecations

--- contrib/roost_pylint_init.py
+++
@@ -1,8 +0,0 @@
-# This file is imported for side-effects in the pylintrc init hook.
-# This lets us add more to the init hook without having it grow
-# unbounded.
-
-import os
-
-# Assume the usual django config if unspecified.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roost_ng.settings')
