73db5d11ac6388ef6fb1c04c79cade539901067f
Alex Dehnert <adehnert@mit.edu>
2024-04-20 18:32:37 +0000

    Add punt hook to drop admin-chosen messages
    Useful if there's a class with some useful messages, but also a high
    rate of messages that nobody needs to see that unnecessarily load the
    server.

--- roost_ng/settings/roost.py
+++ roost_ng/settings/roost.py
@@ -13,3 +13,13 @@ ROOST_USER_CREATION_DENYLIST = cfg.get('user_denylist', [])
 # Making this operator tweakable so we can experiment.
 # JS roost had a 100 message limit. Snipe asked for 128 messages.
 ROOST_MESSAGES_MAX_LIMIT = cfg.get('message_max_limit', 128)
+
+def ROOST_CHECK_PUNT(znotice):
+    """Hook to optionally drop certain messages before adding to DB
+
+    Useful if there's a class with some useful messages, but also a high
+    rate of messages that nobody needs to see that unnecessarily load the
+    server.
+
+    Returns a bool indicating whether or not to punt a message."""
+    return False
